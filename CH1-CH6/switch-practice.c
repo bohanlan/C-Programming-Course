2e83374ca92aa81e7ea327f22bf797bac0fbc9e6
Daniel Lan <bohan.lan2k@gmail.com>
2025-04-29 16:12:41 +0800

    Create switch-practice.c

---
+++ CH1-CH6/switch-practice.c
@@ -0,0 +1,15 @@
+int day = 3;
+switch (day)
+{
+case 1:
+    printf("星期一\n");
+    break;
+case 2:
+    printf("星期二\n");
+    break;
+case 3:
+    printf("星期三\n");
+    break;
+default:
+    printf("其他\n");
+}
